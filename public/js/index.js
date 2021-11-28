e2a16ebd9334fb6ef6397084204fd4715eba99e6
tannerfronk <tannerfronk@gmail.com>
2021-11-28 16:57:30 -0700

    added search bar and created generate token function

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,20 @@
 (function (window) {
+    let searchField = document.querySelector('#gameSearchField')
+    let searchBtn = document.querySelector('#gameSearchBtn')
+    let accessToken // initialize but don't assign until it has been generated
+
+    function getAccessToken() {
+        fetch('/.netlify/functions/generateToken')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+    }
+
+    function search() {
+
+    }
+
     console.log('public script running')
 
     fetch('/.netlify/functions/helloWorld')
