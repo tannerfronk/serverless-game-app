cd929b2ee343d1acd0e056318ef23575bc30bbeb
tannerfronk <tannerfronk@gmail.com>
2021-11-28 19:38:37 -0700

    can now search for games

---
+++ functions/searchGames.js
@@ -0,0 +1,29 @@
+const clientID = process.env.TWITCH_CLIENT
+const fetch = require('node-fetch')
+
+exports.handler = async function (event, context) {
+    let searchObj = JSON.parse(event.body)
+    let search = searchObj.keyword
+    let accessToken = searchObj.token
+    let result
+
+    await fetch(`https://api.igdb.com/v4/games?search=${search}&fields=age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_modes,genres,hypes,involved_companies,keywords,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites`,
+        {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Client-ID': clientID,
+                'Authorization': `Bearer ${accessToken}`,
+            }
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(JSON.stringify({ data }))
+            result = data
+        })
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ result })
+    }
+}
--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,11 @@
 (function (window) {
+
+    const clientID = 'm0wt6yc28s2jcn0xgw5jdehpgipc2t'
     let searchField = document.querySelector('#gameSearchField')
     let searchBtn = document.querySelector('#gameSearchBtn')
-    let accessToken // initialize but don't assign until it has been generated
+    let accessToken // initialize but don't assign until it has been generated or found
 
+    // call function to generate access token for IGDB
     function generateAccessToken() {
         fetch('/.netlify/functions/generateToken')
             .then(res => res.json())
@@ -12,12 +15,13 @@
             })
     }
 
+    // call function to retrieve access token from Mongo for IGDB
     function getAccessToken() {
         fetch('/.netlify/functions/getToken')
             .then(res => res.json())
             .then(data => {
                 console.log(data)
-                if(data.message === 'token expired'){
+                if (data.message === 'token expired') {
                     generateAccessToken()
                 } else {
                     accessToken = data.token
@@ -26,8 +30,30 @@
     }
     getAccessToken()
 
+    // search function
     function search() {
 
+        let searchObj = {
+            keyword: searchField.value,
+            token: accessToken
+        }
+
+        fetch('/.netlify/functions/searchGames', {
+            method: 'POST',
+            body: JSON.stringify(searchObj)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data.result)
+            })
     }
+
+    // event listeners for search function
+    searchBtn.addEventListener('click', search)
+    searchField.addEventListener('keydown', (e) => {
+        if (e.key === 'Enter') {
+            search()
+        }
+    })
 
 })(window)
